376f44c1aa23ce324d45cdfacd44aea8d8e7740d
Lucas X E Pereira <lucasebling@usp.br>
2021-09-17 12:24:10 -0400

    Began *my* version of the main training functions and some accompaning functions
    Started my own training function together with some other functions that will be used by it. I am taking a fragmented approach to the implementation, creating a separate function for every operation we might need.

--- 01/test.m
+++ 01/test.m
@@ -1,9 +1,67 @@
-a = 10
+clc
 
-[a,b] = changeA(a);
-[a,b] = changeA(a);
-a
-b
+prototypes = [2 2 1;10 10 1; 0 0 2; 1 1 2];
+dataPoint = [1 1 1];
+dataPoints = [1 1 1; 2 2 1];
+
+winner  = findClosest(dataPoint, prototypes,4);
+
+is_it_right = correspondant(dataPoint,prototypes,4)
+
+correctness = findCorrectPCTG(dataPoints,prototypes,4,2)
+
+function winner = findClosest(dataPoint, prototypes, K)
+    %some unimaginably far away distance.
+    minDist = 100000;
+    winner = 0;
+    %iterate over protoypes
+    for i = 1:K
+        %Calculate distance
+        %Here we are using the squared euclidean distance.
+        newDist = (dataPoint(1)- prototypes(i,1)).^2 + (dataPoint(2) - prototypes(i,2)).^2;
+        %if the new distance is smaller than the previous one, change it
+        %and update the "winner"
+        if newDist < minDist
+            minDist = newDist;
+            winner = i;
+        end
+        
+    end
+
+end 
+
+%Finds the answer and if the answer is correct.
+%Returns boolean x, True if the number corresponds, False if not
+function x = correspondant(dataPoint, prototypes, K)
+
+    %first we find the closes of the members to the datapoint.
+    winner = findClosest(dataPoint, prototypes,K);
+    
+    %Does it match?
+    if dataPoint(3) == prototypes(winner,3)
+        x = true;
+    else
+        x = false;
+    end 
+
+end 
+
+
+function correct = findCorrectPCTG(dataPoints, prototypes, K, size)
+    
+    curCorrect = 0;
+    
+    for i = 1:size
+        %If your class is the right one, add to curError.
+        if correspondant(dataPoints(i,:),prototypes,K)
+            curCorrect = curCorrect + 1; 
+        end 
+    end 
+    
+    correct = curCorrect/size;
+
+end 
+
 
 %Note = Functions do not change the value of previously assigned variables
 %outside of themselves.s
